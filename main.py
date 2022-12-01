707eb1de34729b7e540047385436ed675e851089
gabsemus <88344550+gabsemus@users.noreply.github.com>
2022-12-01 17:01:05 -0300

    a

--- main.py
+++ main.py
@@ -5,13 +5,38 @@ st.title("Bissoni Record's")
 
 st.header("Perguntas: ")
 
-pergunta1 = st.radio('Você sabia que eu te amo?', ['sim', 'não', 'hmmm'], 2)
+opcoes = ['sim', 'não', 'hmmmm']
 
-match pergunta1:
-    case 'sim':
-        st.write(':fire::sparkling_heart::fire: Weeeeeeeeeeeeeeeeeeeeeeeee :fire::sparkling_heart::fire:')
-    case 'não':
-        st.write(':angry::angry::angry:É OQQQQQQQ????:angry::angry::angry:')
-        st.write('TU TÁ DE SACANAGEM LUIZA BISSONI?')
-    case 'hmmm':
-        st.write('   ...sem pressão')
+pergunta1 = st.radio('Você sabia que eu te amo?', opcoes, 2)
+
+if pergunta1 == opcoes[0]:
+    st.write(':fire::sparkling_heart::fire: Weeeeeeeeeeeeeeeeeeeeeeeee :fire::sparkling_heart::fire:')
+elif pergunta1 == opcoes[1]:
+    st.write('TU TÁ DE SACANAGEM LUIZA BISSONI?')
+else:
+    st.write('')
+
+
+aquela_carinha = """
+░░░░░░░░░░░░▄▄▄▄░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄
+░░░█░░░░▄▀█▀▀▄░░▀▀▀▄░░░░▐█░░░░░░░░░▄▀█▀▀▄░░░▀█▄
+░░█░░░░▀░▐▌░░▐▌░░░░░▀░░░▐█░░░░░░░░▀░▐▌░░▐▌░░░░█▀
+░▐▌░░░░░░░▀▄▄▀░░░░░░░░░░▐█▄▄░░░░░░░░░▀▄▄▀░░░░░▐▌
+░█░░░░░░░░░░░░░░░░░░░░░░░░░▀█░░░░░░░░░░░░░░░░░░█
+▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█
+▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█
+░█░░░░░░░░░░░░░░░░░░░░█▄░░░▄█░░░░░░░░░░░░░░░░░░█
+░▐▌░░░░░░░░░░░░░░░░░░░░▀███▀░░░░░░░░░░░░░░░░░░▐▌
+░░█░░░░░░░░░░░░░░░░░▀▄░░░░░░░░░░▄▀░░░░░░░░░░░░█
+░░░█░░░░░░░░░░░░░░░░░░▀▄▄▄▄▄▄▄▀▀░░░░░░░░░░░░░█
+"""
+
+
+pergunta2 = st.radio('Ctz?', opcoes, 2)
+if pergunta2 == opcoes[0]:
+    st.write(aquela_carinha)
+elif pergunta2 == opcoes[1]:
+    st.write(':angry::angry::angry:É OQQQQQQQ????:angry::angry::angry:')
+else:
+    st.write('')
+
